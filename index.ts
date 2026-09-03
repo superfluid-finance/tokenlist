5054ed8f1763d24b0e2ea64331c93c2623be6b10
Kaspar Kallas <git@kasparkallas.com>
2026-09-03 14:33:56 +0300

    fix: link Arc USDCx underlying token

--- index.ts
+++ index.ts
@@ -8,7 +8,11 @@ export type SuperTokenExtensions = {
   readonly extensions: {
     readonly superTokenInfo:
       | {
-          readonly type: "Pure" | "Native Asset";
+          readonly type: "Pure";
+        }
+      | {
+          readonly type: "Native Asset";
+          readonly underlyingTokenAddress?: `0x${string}`;
         }
       | {
           readonly type: "Wrapper";
